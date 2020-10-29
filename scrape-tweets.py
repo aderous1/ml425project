f4f5cdd92e3c23ad3807d50b26fd876123a06afe
Ben Klein <robobenklein@gmail.com>
2020-10-29 15:03:15 -0400

    let the flood begin
    *more like the rate limiting

--- scrape-tweets.py
+++ scrape-tweets.py
@@ -4,6 +4,7 @@ import os
 import json
 import argparse
 import csv
+from pathlib import Path
 
 import tweepy
 import pandas as pd
@@ -15,7 +16,7 @@ auth = tweepy.OAuthHandler(creds['consumer_key'], creds['consumer_secret'])
 auth.set_access_token(creds['access_key'], creds['access_secret'])
 api = tweepy.API(auth)
 
-def get_tweets(username):
+def get_tweets(username, outfile):
     #set count to however many tweets you want
     number_of_tweets = 1000
 
@@ -26,20 +27,20 @@ def get_tweets(username):
         tweets_for_csv.append([username, tweet.id_str, tweet.created_at, tweet.full_text])
 
     #write to a new csv file from the array of tweets
-    outfile = f"data/tweets/{username}.csv"
     print(f"writing to {outfile}")
-    with open(outfile, 'w+') as file:
+    with outfile.open('w+') as file:
         writer = csv.writer(file, delimiter=',', lineterminator="\n")
         writer.writerows(tweets_for_csv)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        "username",
-        type=str,
-        help="Twitter handle"
-    )
-    args = parser.parse_args()
-
-    get_tweets(args.username)
+    with open('data/congresshandles.json', 'r') as f:
+        handles = json.load(f)
+    handles = [x.replace('@', '') for x in handles]
+    for username in handles:
+        outfile = Path(f"data/tweets/{username}.csv")
+        if outfile.exists():
+            print(f"{username}'s tweets already fetched in {outfile}, skipping")
+        else:
+            print(f"fetching {username}'s tweets ...")
+            get_tweets(username, outfile)
